e8356bbf142b3197b3553cdd9232620e13cfe0ff
Mahmoud Gatfi <mahmoud.gatfi@schibsted.com>
2020-05-30 20:21:06 +0200

    fix

--- markdown-previewer/src/redux/reducers.js
+++ markdown-previewer/src/redux/reducers.js
@@ -2,20 +2,32 @@ import { UPDATE } from "./actions";
 import marked from "marked";
 
 const initialInput =
-  "# This is my title\n" +
-  "## This my subtitle\n" +
-  "**bold text**\n" +
-  "_italic text_\n\n" +
-  "**Lists**\n" +
-  "* Item 1\n" +
-  "* Item 2\n" +
-  "  * Item 2a\n" +
-  "  * Item 2b\n" +
-  "1. Item 1\n" +
-  "1. Item 2\n" +
-  "1. Item 3\n" +
-  "   1. Item 3a\n" +
-  "   1. Item 3b\n";
+  "# This is my title" +
+  "\n## This my subtitle" +
+  "\n**bold text**" +
+  "\n_italic text_" +
+  "\n**_image_**" +
+  "\n" +
+  "\n![Mignon chaton](https://www.manga-news.com/public/images/dvd/chi-une-vie-de-chat-dvd-glenat.jpg)" +
+  "\n" +
+  "\n**Lists**" +
+  "\n* Item 1" +
+  "\n* Item 2" +
+  "\n  * Item 2a" +
+  "\n  * Item 2b" +
+  "\n" +
+  "\n**_link_** [FreeCodeCamp](http://https://www.freecodecamp.org)" +
+  "\n" +
+  "\n_**une ligne de code**_ " +
+  "\n` <div> ma div </div> `" +
+  "\n" +
+  "\n_**Plusieurs lignes de code**_" +
+  "\n```" +
+  "\nfunction add(a,b) {" +
+  "\n   return a+b;" +
+  "\n}" +
+  "\n```" +
+  "\n";
 
 export const initialState = {
   input: initialInput,
